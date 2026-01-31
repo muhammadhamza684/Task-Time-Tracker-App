ff6699d38c192b97308b0380cd5edaa35cb29d66
Muhammad Hamza <muhammadhamzasw517@gmail.com>
2026-01-31 16:20:22 +0500

    master Testing

---
+++ Models/masterTEsting.cs
@@ -0,0 +1,9 @@
+﻿namespace Task___Time_Tracker_App.Models
+{
+    public class masterTEsting
+    {
+        public int master1 { get; set; }
+
+        public int master2 { get; set; }    
+    }
+}
