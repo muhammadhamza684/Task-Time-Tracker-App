3f8aca011ec43a79b868d3ac33057d14ff8b9265
Muhammad Hamza <muhammadhamzasw517@gmail.com>
2026-01-31 16:47:42 +0500

    Updated For Testing master to gittesting

---
+++ Models/masterModel1.cs
@@ -0,0 +1,11 @@
+﻿namespace Task___Time_Tracker_App.Models
+{
+    public class masterModel1
+    {
+        public int MyProperty1 { get; set; }
+
+        public int MyProperty2 { get; set; }
+
+        public int MyProperty454 { get; set; }
+    }
+}
---
+++ Models/masterModel2.cs
@@ -0,0 +1,9 @@
+﻿namespace Task___Time_Tracker_App.Models
+{
+    public class masterModel2
+    {
+        public int MyProperty1 { get; set; }
+
+        public int MyProperty22 { get; set; }
+    }
+}
